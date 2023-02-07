9b24882c564e9f76f0a9612dc388d5d90f32718c
DevvSakib <devvsakib@gmail.com>
2023-02-07 16:50:05 +0600

    total project count added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import GetProjects from '../components/Projects/GetProjects'
-// import ProjectBG from '../components/Projects/ProjectBG'
+import { projects } from '../../content/projects.json';
 const Projects = () => {
 
     return (
-        <section className='grid z-999 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 py-5 gap-10 bg-gradient-to-r px-0'>
-            <div className="circle -z-99999 "></div>
-            <div className="circle -z-99999 c2"></div>
-            <div className="circle -z-99999 c3"></div>
-
-            <GetProjects />
-            {/* <ProjectBG /> */}
-        </section>
+        <>
+            <h3 className='text-center md:text-left'>Total Project: <span className='font-bold text-[#07C5D1]'>{projects.length}</span></h3>
+            <section className='grid z-999 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 py-5 gap-10 bg-gradient-to-r px-0'>
+                <div className="circle -z-99999 "></div>
+                <div className="circle -z-99999 c2"></div>
+                <div className="circle -z-99999 c3"></div>
+                <GetProjects />
+            </section>
+        </>
     )
 }
 
