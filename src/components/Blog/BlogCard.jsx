436e1fc5ef7c1f26b7f4450dc337969d724c92d4
DevvSakib <devvsakib@gmail.com>
2023-02-11 20:12:10 +0600

    blog added

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -25,23 +25,29 @@ const BlogCard = () => {
             {
                 blogs.map(blog => (
 
-                        <div className='backdrop-blur-[5px] bg-white/5 shadow-[#851EED]/30 shadow-md p-4 rounded-lg'>
-                            <div className='h-[300px] overflow-hidden rounded-lg'>
-                                <img className='w-full h-full object-cover object-top' src={blog.thumbnail} alt="" />
-                            </div>
-                            <div className="bg-transparent">
-                                <h2 className='text-xl text-[#07C5D1] mt-5 mb-2 '>{blog.title}</h2>
-                                <p className='text-sm text-[#b274f0]'>{blog.content}</p>
-                            </div>
-                            <div className="bg-transparent flex gap-5 mt-4">
-                                <button className='bg-[#07C5D1]/20 backdrop-blur-[5px] px-5 py-1 rounded-full text-[#07C5D1]'>
-                                    <a href={blog.url}>Read</a>
-                                </button>
-                                <button className='bg-[#851EED]/20 backdrop-blur-[5px] px-5 py-1 rounded-full text-[#851EED]'>
-                                    <a href={blog.url}>Share</a>
-                                </button>
-                            </div>
+                    <div className='backdrop-blur-[5px] bg-white/5 shadow-[#851EED]/30 shadow-md p-4 rounded-lg h-auto sm:h-[440px]'>
+                        <div className=' h-[150px] sm:h-[200px] w-full md:h-[50%] overflow-hidden rounded-lg'>
+                            <img className='w-full object-cover skeleton' src={blog.thumbnail} alt="" />
                         </div>
+                        <div className="bg-transparent">
+                            <h2 className='text-lg text-[#07C5D1] mt-5 mb-2 '>{blog.title}</h2>
+                            <p className='text-sm text-[#b274f0]'>
+                                {
+                                    blog.content.length > 120
+                                    &&
+                                    blog.content.slice(0, 120) + "..."
+                                }
+                            </p>
+                        </div>
+                        <div className="bg-transparent flex gap-5 mt-4">
+                            <button className='bg-[#07C5D1]/20 backdrop-blur-[5px] px-5 py-1 rounded-full text-[#07C5D1]'>
+                                <a href={blog.url} target="_blank">Read</a>
+                            </button>
+                            <button className='bg-[#851EED]/20 backdrop-blur-[5px] px-5 py-1 rounded-full text-[#851EED]'>
+                                <a href={blog.url}>Share</a>
+                            </button>
+                        </div>
+                    </div>
                 ))
             }
         </motion.div>
