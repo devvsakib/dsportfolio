897ca5d26453e4a6b8b49f10ad0fb5c02fb08d79
DevvSakib <devvsakib@gmail.com>
2023-02-15 23:20:01 +0600

    catty design

--- src/components/Notice/Notice.jsx
+++ src/components/Notice/Notice.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Notice = () => {
   return (
     <div className='absolute bg-transparent top-20 right-10 md:right-36 md:top-28 text-center'>
-        <span className='absolute top-12 md:top-14  text-[8px] md:text-[10px] left-[24px] md:left-[29px] skew-x-12 skew-y-12'>Coding..</span>
+        <span className='text-black absolute top-12 md:top-14  text-[8px] md:text-[10px] left-[24px] md:left-[29px] skew-x-12 skew-y-12'>Coding..</span>
         <img src="/assets/cat.gif" className='w-32 md:w-40' alt="" />
-        <p>Building in progrress...</p>
+        <p className="text-[12px]">Building in progress...<br/>Ignore me</p>
     </div>
   )
 }
