1564580e46e457cc6f54dd2f2f7d325efad94ed5
DevvSakib <devvsakib@gmail.com>
2023-02-15 21:06:14 +0600

    coding test in laptop back

--- src/components/Notice/Notice.jsx
+++ src/components/Notice/Notice.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Notice = () => {
   return (
-    <div className='absolute top-20 right-10 md:right-36 md:top-28 text-center'>
+    <div className='absolute z-[99999] bg-transparent top-20 right-10 md:right-36 md:top-28 text-center'>
+        <span className='absolute top-12 md:top-14  text-[8px] md:text-[10px] left-[24px] md:left-[29px] skew-x-12 skew-y-12'>Coding..</span>
         <img src="/assets/cat.gif" className='w-32 md:w-40' alt="" />
         <p>Building in progrress...</p>
     </div>
