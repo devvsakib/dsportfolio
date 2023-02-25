764ff788822ef8301493d8bd8d15543cf96ab219
DevvSakib <devvsakib@gmail.com>
2023-02-25 12:02:52 +0600

    stack btns space and grid

--- src/components/About/Stack.jsx
+++ src/components/About/Stack.jsx
@@ -3,10 +3,10 @@ const Stack = () => {
     const stacksBackend = ["Node.js", "Express", "MongoDB", "RestAPI"]
     return (
 
-        <div className="mt-5 mb-20  grid grid-cols-1 lg:grid-cols-2  gap-5">
+        <div className="mt-5 mb-20  grid grid-cols-1 lg:grid-cols-2  gap-y-10 gap-x-32">
             <div className="flex flex-col">
                 <h2 className="devv-title text-left text-4xl inline-block">Frontend</h2>
-                <div className="flex flex-wrap gap-5 mt-5">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
                     {
                         stacksFrontend?.map((item, idx) => (
                             <button key={idx} className="bg-gradient-to-bl from-[#07C5D1]/50 to-[#E02AAD]/50 text-white px-5 py-2 rounded shadow-black/60 shadow-md block">{item}</button>
@@ -16,7 +16,7 @@ const Stack = () => {
             </div>
             <div className="flex flex-col gap-5">
                 <h2 className="text-left devv-title text-4xl inline-block">Backend</h2>
-                <div className="flex flex-wrap gap-5">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5">
 
                     {
                         stacksBackend?.map((item, idx) => (
