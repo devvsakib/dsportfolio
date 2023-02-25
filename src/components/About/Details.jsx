6a6448ce5149dd6781fa8f451e3db6b43ba26f43
DevvSakib <devvsakib@gmail.com>
2023-02-25 12:13:17 +0600

    cv added

--- src/components/About/Details.jsx
+++ src/components/About/Details.jsx
@@ -5,8 +5,11 @@ const Details = () => {
     return (
         <div className="mt-10 z-[9999999999999999999999999]">
             <div className="grid grid-cols-1 md:grid-cols-2  grid-rows-1  items-center gap-10">
-                <div className="rounded-full overflow-hidden bg-gradient-to-r  via-[#ffe700]/5 from-[#851EED]/5 to-[#07C5D1]/5 devv-bg">
-                    <img className="w-1/2 mx-auto rounded-full" src="/assets/DevvSakib.png" alt="" />
+
+                <div className="justify-center">
+                    <div className="rounded-full overflow-hidden bg-gradient-to-r  via-[#ffe700]/5 from-[#851EED]/5 to-[#07C5D1]/5 devv-bg">
+                        <img className="w-1/2 mx-auto rounded-full" src="/assets/DevvSakib.png" alt="" />
+                    </div>
                 </div>
                 <div className="text-md tracking-wider font-[poppins] text-white">
                     <p className="mb-3">I am a <span>Frontend Developer</span> with a working proficiency in React, HTML, CSS, JavaScript. Contributting to Open-Source daily. Open-source is the best way to connect with new people and it help to gain more knowledge.</p>
@@ -15,7 +18,14 @@ const Details = () => {
                         <p className="devvTextAnimation">Looking for the right opportunity. If you want you can contact with me.</p>
                     </div>
                 </div>
-                <SocialLinks/>
+                <div className="text-center md:col-span-2  mt-3">
+                    <button className="bg-gradient-to-bl from-[#07C5D1]/50 to-[#E02AAD]/50 text-white px-5 py-2 rounded  shadow-black/60 shadow-md">
+                        <a href="/assets/devvsakib.pdf" download>
+                        Download CV
+                        </a>
+                        </button>
+                </div>
+                <SocialLinks />
             </div>
             <div className="text-center  mt-40">
                 <h2 className="devv-title text-3xl md:text-6xl inline">Stacks</h2>
