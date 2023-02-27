cd13447efe397067c0ef817250e55b3dd2b10058
DevvSakib <devvsakib@gmail.com>
2023-02-27 21:25:01 +0600

    redirect file added, 404 page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { useState, useEffect } from "react";
 import './css/header.css'
 import MenuIcon from '@mui/icons-material/Menu';
